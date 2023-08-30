dcc6caae2050302c2855f4bad746db940f2ee62f
lutfiandri <lutfiandri232@gmail.com>
2023-08-30 11:59:58 +0700

    feat(vercel): add express for ping connection

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const env_1 = __importDefault(require("./configs/env"));
 const mqtt_client_1 = __importDefault(require("./configs/mqtt-client"));
 require("./routes/telemetry-routes");
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 mqtt_client_1.default.on('connect', () => {
     mqtt_client_1.default.subscribe(env_1.default.MqttTopic, (err) => {
         if (!!err) {
@@ -14,4 +16,10 @@ mqtt_client_1.default.on('connect', () => {
         console.log(`mqtt connected to ${env_1.default.MqttUrl} on topic ${env_1.default.MqttTopic}`);
     });
 });
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.get('/', (req, res) => res.send('welcome to embersense'));
+app.listen(5000, () => console.log('server started'));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import env from './configs/env';
 import mqttClient from './configs/mqtt-client';
 import './routes/telemetry-routes';
 
+import express from 'express';
+import cors from 'cors';
+
 mqttClient.on('connect', () => {
   mqttClient.subscribe(env.MqttTopic, (err) => {
     if (!!err) {
@@ -10,3 +13,13 @@ mqttClient.on('connect', () => {
     console.log(`mqtt connected to ${env.MqttUrl} on topic ${env.MqttTopic}`);
   });
 });
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => res.send('welcome to embersense'));
+
+app.listen(5000, () => console.log('server started'));
